9ff001388967a4df5e98c27f3b42ca1f68bf8339
xxabbu <ashish.raj@gmail.com>
2018-09-14 16:37:55 -0700

    added sw testing

--- demean_age.py
+++ demean_age.py
@@ -1,6 +1,6 @@
-Debugger entered--Lisp error: (void-variable import)
-  eval-region(1 198 t (lambda (ignore) (goto-char 198) (quote import)))  ; Reading at buffer position 193
-  apply(eval-region (1 198 t (lambda (ignore) (goto-char 198) (quote import))))
-  eval-defun-2()
-  eval-defun(nil)
-  call-interactively(eval-defun)
+import numpy as np
+
+age = np.loadtxt("participants.tsv",skiprows=1,usecols=1)
+mean_age = sum(age)/len(age)
+np.savetxt("demeaned_age.txt",age - mean_age)
+print("done!")
---
+++ demean_age.py~
@@ -0,0 +1,6 @@
+Debugger entered--Lisp error: (void-variable import)
+  eval-region(1 198 t (lambda (ignore) (goto-char 198) (quote import)))  ; Reading at buffer position 193
+  apply(eval-region (1 198 t (lambda (ignore) (goto-char 198) (quote import))))
+  eval-defun-2()
+  eval-defun(nil)
+  call-interactively(eval-defun)
